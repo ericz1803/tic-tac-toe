41213de31335bf954c7f36e2c0e4b89db6b43c2f
Eric Zhou <ericz1803@gmail.com>
2019-10-22 15:09:02 -0700

    updated script

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:ericz1803/tic-tac-toe.git master:gh-pages
 
 cd -
